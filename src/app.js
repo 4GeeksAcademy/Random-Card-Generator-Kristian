bb06d91fabab8e7ffd6abd6c1114487928ae0e8b
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-05 03:55:49 +0000

    Updated visual card, items position and added button

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
-import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
   //write your code here
   let values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   let cardValues = values[Math.floor(Math.random() * values.length)];
-  console.log(cardValues);
   
   let pips = ["♥", "♦", "♣", "♠"];
   let cardPips = pips[Math.floor(Math.random() * pips.length)];
-  console.log(cardPips);
+  let color = "";
+  
+  if (cardPips === "♥" || cardPips === "♦"){
+    color = "red";
+  } else {
+    color = "black";
+  }
+
+  document.getElementById("center").innerHTML = cardValues;
+  document.querySelectorAll(".pips").forEach(figure => {
+    figure.innerHTML = cardPips
+    figure.style.color = color
+    })
 };
